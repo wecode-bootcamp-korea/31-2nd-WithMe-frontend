63192606adea52de61b3a70690d00fd2ac90fe54
kyuhyun ro <kyuhyunro@kyuhyunui-MacBookPro.local>
2022-04-11 22:22:58 +0900

    FIX: index.js 파일 수정

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import Router from './Router';
+import GlobalStyles from './styles/GlobalStyles';
+import theme from './styles/theme';
+
 const rootNode = document.getElementById('root');
-ReactDOM.createRoot(rootNode).render(<Router />);
+ReactDOM.createRoot(rootNode).render(
+  <>
+    <GlobalStyles />
+    <ThemeProvider theme={theme}>
+      <Router />
+    </ThemeProvider>
+  </>
+);
