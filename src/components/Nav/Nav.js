28b41b7b112e7c52d47bc38b3d10c75f7085f3a4
Kyungsuh Park <pyo05360@gmail.com>
2022-04-13 15:12:36 +0900

    Add: nav 레이아웃 추가

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,79 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const Nav = () => {
-  return <div>응애 네브</div>;
+  return (
+    <Container>
+      <NavWrap>
+        <Menu>
+          <Link to="/">
+            <img src="/images/Nav/logo.png" alt="logo" />
+          </Link>
+
+          <NavBox>
+            <Link to="/discovery">놀이터 둘러보기</Link>
+            <Link to="/host">놀이터 등록하기</Link>
+            <Link to="/about">놀이터란</Link>
+          </NavBox>
+        </Menu>
+
+        <Mapage>
+          <Link to="/search">
+            <img src="/images/Nav/search.png" alt="search" />
+          </Link>
+          <Link to="/login">로그인/회원가입</Link>
+        </Mapage>
+      </NavWrap>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  position: relative;
+  height: 67px;
+  border-bottom: 1px solid #ddd;
+  background-color: #fff;
+`;
+
+const NavWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 89vw;
+  max-width: 1140px;
+  margin: 0 auto;
+`;
+
+const Menu = styled.div`
+  display: flex;
+  gap: 6rem;
+
+  img {
+    width: 19px;
+    height: 30px;
+  }
+`;
+
+const NavBox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 25px;
+  font-weight: 300;
+`;
+
+const Mapage = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 16px;
+  font-weight: 300;
+
+  img {
+    width: 25px;
+    height: 25px;
+  }
+`;
 
 export default Nav;
